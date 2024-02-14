c618137e8aa84065839132d470ba989fa1f38c53
elsa hadjadj <ehadjadj@iut.univ-paris8.fr>
2024-02-14 01:01:39 +0100

    Fisrt commit

---
+++ src/machine/GameMachine.ts
@@ -0,0 +1,54 @@
+import { createMachine } from "xstate";
+
+enum GameStates {
+  LOBBY = 'LOBBY',
+  PLAY = 'PLAY',
+  VICTORY = 'VICTORY',
+  DRAW = 'DRAW'
+}
+export const GameModel = createModel({
+  players: [],
+})
+
+export const GameMachine = createMachine({
+  id:'game',
+  initial: GameStates.LOBBY,
+  states: {
+    [GameStates.LOBBY]: {
+      on: {
+        join: {
+          target: GameStates.LOBBY
+        },
+        leave: {
+          target: GameStates.LOBBY
+        },
+        chooseColor: {
+          target: GameStates.LOBBY
+        },
+        start: {
+          target:GameStates.PLAY 
+        } 
+      }
+  },
+  [GameStates.PLAY]: {
+    on: {
+      dropToken: {
+        target: '????'
+      } 
+    }
+  },
+  [GameStates.VICTORY]: {
+    on: {
+      restart: {
+        target: GameStates.LOBBY
+      }
+    }
+  },
+  [GameStates.DRAW]: {
+    on: {
+      restart: {
+        target: GameStates.LOBBY
+      }
+    }
+  }
+})
